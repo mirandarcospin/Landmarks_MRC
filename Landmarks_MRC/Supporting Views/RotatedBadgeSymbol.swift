763e8ff913e0fb7d48eb9e7b041fbc5afc4aeca2
Miranda Ramirez Cospin <mxr17e@acu.edu>
2020-11-05 00:58:22 -0600

    Chapter 2: Drawing Paths and Shapes

---
+++ Landmarks_MRC/Supporting Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks_MRC
+//
+//  Created by Miranda Ramirez Cospin on 11/5/20.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
