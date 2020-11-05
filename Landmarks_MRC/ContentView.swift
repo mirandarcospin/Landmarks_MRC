8c10f87da928dbe21811320d761d823fb176de57
Miranda Ramirez Cospin <mxr17e@acu.edu>
2020-11-04 19:58:10 -0600

    Chapter 1: Creating and Combining Views

--- Landmarks_MRC/ContentView.swift
+++ Landmarks_MRC/ContentView.swift
@@ -9,18 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            //Adding map view
+            MapView()
+                .edgesIgnoringSafeArea(.top) //to extend map to the top edges of the scren
+                .frame(height: 300)
+            //Adding circle image and position
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            //Text
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack (alignment: .top){
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
             }
+            .padding()
+            //Pushing content to the top of the screen
+            Spacer()
         }
-        .padding()
     }
 }
 
