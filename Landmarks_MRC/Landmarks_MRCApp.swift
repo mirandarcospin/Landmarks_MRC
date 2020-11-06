150801332e0d4519aaf44ef20bcf3dce980b8b43
Miranda Ramirez Cospin <mxr17e@acu.edu>
2020-11-06 17:26:46 -0600

    Chapter 3: Working with UI Controls

---
+++ Landmarks_MRC/Landmarks_MRCApp.swift
@@ -0,0 +1,18 @@
+//
+//  Landmarks_MRCApp.swift
+//  Landmarks_MRC
+//
+//  Created by Miranda Ramirez Cospin on 11/6/20.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_MRC: App {
+    var body: some Scene {
+        WindowGroup {
+            LandmarkList()
+        }
+    }
+}
+
